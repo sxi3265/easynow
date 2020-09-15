b086e7a8bbe95816091c8cd3057649df171c00c1
sxi3265 <sxi3265@gmail.com>
2020-09-15 11:01:27 +0800

    完成什么值得买签到

--- EasyNow.Job.Smzdm/Program.cs
+++ EasyNow.Job.Smzdm/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using EasyNow.Utility.Extensions;
+using Microsoft.Extensions.Configuration;
 using PuppeteerSharp;
 using PuppeteerSharp.Input;
 
@@ -7,36 +10,38 @@ namespace EasyNow.Job.Smzdm
 {
     class Program
     {
+        private static IConfigurationRoot Configuration { get; set; }
         static async Task Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            var launchOptions = new LaunchOptions
             {
-                Headless = false
-            });
+                Headless = false,
+                // Args = new []{"--no-sandbox"},
+                IgnoredDefaultArgs = new []{"--enable-automation"},
+                DefaultViewport = new ViewPortOptions
+                {
+                    Width = 1920,
+                    Height = 1080
+                }
+            };
+            var browser = await Puppeteer.LaunchAsync(launchOptions);
             var page = await browser.NewPageAsync();
-            await page.SetViewportAsync(new ViewPortOptions
-            {
-                Width = 1920,
-                Height = 1080
-            });
-            //https://zhiyou.smzdm.com/user/login/
+            await page.SetUserAgentAsync(Configuration["UserAgent"]);
+            await page.SetCookieAsync(Configuration["Cookies"].FromJson<CookieParam[]>());
             await page.GoToAsync("https://www.smzdm.com/");
-            var element=await page.WaitForSelectorAsync("a.J_login_trigger");
-            await element.ClickAsync();
-            element = await page.WaitForSelectorAsync("iframe#J_login_iframe");
-            var frame = await element.ContentFrameAsync();
-            
-            element = await frame.WaitForSelectorAsync("div.qrcode-change.J_qrcode_change");
-            var box = await element.BoundingBoxAsync();
-            await Task.Delay(5000);
-            await page.Mouse.ClickAsync(box.X + box.Width / 3 * 2, box.Y + box.Height / 3);
-            element = await frame.WaitForSelectorAsync("input#username");
-            await frame.TypeAsync("input#username", "xxxxxx", new TypeOptions
+            var element =await page.WaitForSelectorAsync("a.J_punch");
+            var text=await (await element.GetPropertyAsync("textContent")).JsonValueAsync<string>();
+            // var b=await page.EvaluateFunctionAsync<string>("e=>e.textContent",element);
+            if (text == "签到领奖")
             {
-                Delay = 100
-            });
-            Console.WriteLine("Hello World!");
+                await element.ClickAsync();
+            }
         }
     }
 }
