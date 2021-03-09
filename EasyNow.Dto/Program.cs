05db31caa229433f2059018ee51ce32564ae12c5
sxi3265 <sxi3265@gmail.com>
2021-03-09 21:24:11 +0800

    添加项目文件。

---
+++ EasyNow.Dto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyNow.Dto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
